b67c4f9cf602687ae6dc829a234ebec1ef50315c
solitaire <fmailiulian@gmail.com>
2026-04-21 20:51:22 +0300

    clean console

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { colorize } from "./ui/style.console.js";
 import { initModules } from "./modules/modules.js";
 
 export async function __MAIN() {
+  console.clear();
   BANNER();
 
   await initModules();
